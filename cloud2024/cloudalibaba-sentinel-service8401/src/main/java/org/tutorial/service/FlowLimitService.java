b144c7192b36f6b6f7d63cb6b305335efad4a2b3
連千港 Howard <chienkang1114@gmail.com>
2024-05-03 10:55:05 +0800

    Update FlowLimitService.java

--- cloud2024/cloudalibaba-sentinel-service8401/src/main/java/org/tutorial/service/FlowLimitService.java
+++ cloud2024/cloudalibaba-sentinel-service8401/src/main/java/org/tutorial/service/FlowLimitService.java
@@ -6,6 +6,8 @@ import com.alibaba.csp.sentinel.annotation.SentinelResource;
 
 @Service
 public class FlowLimitService {
+
+	// Sentinel中的一個注解，用於標識資源，以便進行流量控制、熔斷降級等操作
 	@SentinelResource(value = "common")
 	public void common() {
 		System.out.println("------FlowLimitService come in");
